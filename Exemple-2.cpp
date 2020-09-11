b4300b371ed484593da569442e8007996f878d48
Javohir-cryptic <javohirerkinovich@gmail.com>
2020-09-12 00:34:42 +0300

    Initial Commit

---
+++ Exemple-2.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int a, b, c, max;
+	cout << "Введите три целых числа: ";
+	cin >> a >> b >> c;
+
+	if(a >= b && a >= c)
+		max = a;
+	if (b >= c && c >= b)
+		max = b;
+	if (c >= a && c >= b)
+		max = c;
+	cout << "Max: " << max;
+	cout << "\n";
+	return 0;
+}
+
